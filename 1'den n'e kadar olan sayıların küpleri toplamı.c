badb5636b971503e4397ec6c794ffb5c34250345
emirhanlyc <145213538+emirhanlyc@users.noreply.github.com>
2023-11-02 15:18:47 +0300

    1'den n'e kadar olan sayıların küpleri toplamı

---
+++ 1'den n'e kadar olan sayıların küpleri toplamı.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include <locale.h> /* Türkçe karakter kullanýmý için gereken kod*/
+
+#include <conio.h>
+int main() 
+
+{
+setlocale(LC_ALL, "Turkish");  /*Türkçe karakter kullanýmý için gereken kod*/
+
+
+
+	int i, sayi, toplam=0;
+	
+	printf("Lütfen üst limit giriniz : ");
+	scanf("%d", &sayi);
+	
+	for(i=1; i<=sayi; i++)
+	{
+		
+		
+		
+		toplam=toplam+i*i*i;
+	}
+	
+	printf("1'den %d'e kadar olan sayýlarýn küpleri toplamý : %d", sayi, toplam);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
