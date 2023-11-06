da5a6f0e5e8c985613baf252d03e453ad99ab13c
emirhanlyc <145213538+emirhanlyc@users.noreply.github.com>
2023-11-06 21:07:56 +0300

    Fibonacci dizisini veren kod

---
+++ Fibonacci.c
@@ -0,0 +1,1 @@
+#include<stdio.h>int main(){  int a=0, b=1, c, i, n;  printf("Sayı adeti giriniz : "); scanf("%d", &n);for(i=1; i<=n; i++) {printf("%d . sayı = %d\n", i,c);c=a+b;a=b;b=c;    }    }
