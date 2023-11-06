880de9d61d40191844b9691f115ea0a5cd163cbf
emirhanlyc <145213538+emirhanlyc@users.noreply.github.com>
2023-11-06 20:53:02 +0300

    DÖNGÜLÜ Negatif girilene kadar toplama  işlemi

---
+++ DÖNGÜLÜ NEGATİF GİRİLENE KADAR TOPLAMA.c
@@ -0,0 +1,35 @@
+
+
+#include<stdio.h>
+int main()
+{
+int sayi, toplam=0, adet=0, i;
+
+
+for(;; adet++)
+{ 
+    
+    
+     printf("Sayı giriniz : ");
+     scanf("%d", &sayi);
+
+   if(sayi<0)
+   
+  
+       
+       
+       
+     break;
+       
+       toplam=toplam+sayi;
+   
+    
+    
+    
+    
+    
+}
+printf("Sayıların toplamı = %d\n", toplam);
+    printf("Adet= %d\n",adet);
+
+}
