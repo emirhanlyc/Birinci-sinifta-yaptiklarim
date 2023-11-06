97f77ec2d3a1f0061294768804d940681ad2b57f
emirhanlyc <145213538+emirhanlyc@users.noreply.github.com>
2023-11-06 21:44:16 +0300

    Asal sayı kontrolü yapan kod

---
+++ Asal sayı bulucu.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    
+    int i, asal=1, sayi;
+    
+    printf("Sayı girin");
+    scanf("%d", &sayi);
+    
+    for(i=2; i<sayi; i++)
+    
+    if(sayi%i==0){
+        
+    asal=0;
+    break;
+        
+        
+}    
+        
+     if (sayi==i)
+     printf("Asal");
+     else printf("Değil");
+        
+    
+    
+   
+    }
+  
+    
+    
+    
+    
