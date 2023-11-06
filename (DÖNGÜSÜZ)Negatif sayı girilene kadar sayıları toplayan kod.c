4d54c27ba18fde467869c2860375150e556847aa
emirhanlyc <145213538+emirhanlyc@users.noreply.github.com>
2023-11-06 09:46:58 +0300

    İf ile negatif sayı girilene kadar toplayan kod

---
+++ (DÖNGÜSÜZ)Negatif sayı girilene kadar sayıları toplayan kod.c
@@ -0,0 +1,31 @@
+#include <stdio.h>                   
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Turkish");
+
+int sayi, toplam=0;
+
+printf("Toplanacak sayýlarý giriniz : ");
+x : scanf("%d", &sayi);
+
+if(sayi>0){
+
+
+toplam=toplam+sayi;
+    
+    
+    goto x;}
+   
+   
+   else 
+   printf("Sayýlarýn toplamý : %d", toplam);
+  
+   
+   
+   
+   
+   
+   
+}
+
